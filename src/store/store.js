c9af590d054802d590eac46669723f14c2fc438a
MJKM <rhkrrbaudgg@naver.com>
2022-10-18 18:46:03 +0900

    store에 toggle reducer 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import groundSlice from "./reducers/groundSlice";
+import toggleSlice from "./reducers/toggleSlice";
 
 const store = configureStore({
   reducer: {
     ground: groundSlice.reducer,
+    toggle: toggleSlice.reducer,
   },
 });
 
